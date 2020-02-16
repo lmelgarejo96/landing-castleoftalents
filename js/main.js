e65a847cec43a244aca7c80dda41df671c48c2d6
Luis Melgarejo <luismelgarejoalarcon@gmail.com>
2020-02-16 14:17:39 -0500

    quinto avance

--- js/main.js
+++ js/main.js
@@ -24,3 +24,64 @@ $('#videoModal').on('hidden.bs.modal', function(e) {
 
 /* const circleType = new CircleType(document.getElementById('welcome-title'));
 circleType.radius(200); */
+
+
+var enlacesHeader = document.querySelectorAll(".navbar-collapse")[0];
+var semaforo = false;
+
+document.querySelectorAll(".navbar-toggler")[0].addEventListener("click", function() {
+    let nav_btn = document.getElementById("icon-navbar");
+    const classesnav = document.getElementById('menu').classList;
+    if (!semaforo) {
+        nav_btn.style.color = "rgb(255, 255, 255)";
+        nav_btn.classList.remove('fa-bars');
+        nav_btn.classList.add('fa-close')
+        semaforo = true;
+    } else {
+        color = "rgb(0, 0, 0)";
+        classesnav.forEach((item) => {
+            if (item === 'dark-navbar') {
+                color = "rgb(255, 255, 255)";
+            }
+        })
+        nav_btn.classList.remove('fa-close')
+        nav_btn.classList.add('fa-bars');
+        nav_btn.style.color = color;
+        semaforo = false;
+    }
+    enlacesHeader.classList.toggle("menudos");
+});
+
+/* function opcloseMenu() {
+    let nav_btn = document.getElementById("icon-navbar");
+    const classesnav = document.getElementById('menu').classList;
+    if (!semaforo) {
+        nav_btn.style.color = "rgb(255, 255, 255)";
+        nav_btn.classList.remove('fa-bars');
+        nav_btn.classList.add('fa-close')
+        semaforo = true;
+    } else {
+        color = "rgb(0, 0, 0)";
+        classesnav.forEach((item) => {
+            if (item === 'dark-navbar') {
+                color = "rgb(255, 255, 255)";
+            }
+        })
+        nav_btn.classList.remove('fa-close')
+        nav_btn.classList.add('fa-bars');
+        nav_btn.style.color = color;
+        semaforo = false;
+    }
+    enlacesHeader.classList.toggle("menudos");
+} */
+
+function prepareLinks() {
+    const links = document.querySelectorAll(".nav-link")
+    for (i = 4; i < links.length; i++) {
+        links[i].addEventListener("click", function() {
+            const navBtn = document.querySelectorAll(".navbar-toggler")[0];
+            navBtn.click();
+        });
+    }
+}
+prepareLinks();
--- js/scroll.js
+++ js/scroll.js
@@ -1,14 +1,17 @@
 /* JQUERY */
 const wrapper = document.getElementById('wrapper');
+let nav_btn = document.getElementById("icon-navbar");
 $(wrapper).scroll(function(e) {
     let desplazamiento = $(this).scrollTop();
     if (desplazamiento > 400) {
         $('#menu').addClass('dark-navbar');
         $('#contacto').addClass('dark-navbar');
         $('#fondo-blanco').addClass('dark-navbar');
+        nav_btn.style.color = "rgb(255, 255, 255)";
     } else {
         $('#menu').removeClass('dark-navbar');
         $('#contacto').removeClass('dark-navbar');
         $('#fondo-blanco').removeClass('dark-navbar');
+        nav_btn.style.color = "rgb(0, 0, 0)";
     }
 });
